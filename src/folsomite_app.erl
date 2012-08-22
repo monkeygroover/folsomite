dae4a23eb9605d0476d9d5eba49d7a04108aa6db
Can Arel <can@campanja.com>
2012-08-22 15:58:33 +0200

    Changing order of startup in application

--- src/folsomite_app.erl
+++ src/folsomite_app.erl
@@ -20,7 +20,7 @@
 start(normal, no_arg) ->
     folsom_metrics:new_histogram(
       {folsomite, send_stats}, slide_uniform, {60, 1028}),
-    folsomite_sup:start_link(),
-    riemann_error_logger:register_with_logger().
+    riemann_error_logger:register_with_logger(),
+    folsomite_sup:start_link().
 
 stop(_) -> ok.
