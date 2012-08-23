216ec144e2b92de51294bdc58575fddf8a25fb1d
Can Arel <can@campanja.com>
2012-08-23 14:29:26 +0200

    Changed so safe function chops at 1000

--- src/riemann_error_logger.erl
+++ src/riemann_error_logger.erl
@@ -63,7 +63,7 @@ node_prefix() ->
     A.
 
 send_stats(State, Data)->
-    {FmtData, _Used} = lager_trunc_io:safe(Data,250),
+    {FmtData, _Used} = lager_trunc_io:safe(Data, 1000),
     Hostname = net_adm:localhost(),
     Prefix = State#state.node_prefix ++ " ",
     zeta:cv({Hostname, Prefix ++ "erlang crash"}, 1, critical,
