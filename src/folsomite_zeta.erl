4cb13183223e5de355717aaccd2db752a8d0f699
Torbjörn Norinder <torbjorn@campanja.com>
2012-11-08 15:09:50 +0100

    Refactor

--- src/folsomite_zeta.erl
+++ src/folsomite_zeta.erl
@@ -1,16 +1,22 @@
 -module(folsomite_zeta).
--export([event/3, event/4, hostname/0, node_prefix/0]).
+-export([event/3, event/4, host_event/3, host_event/4, get_tags/0]).
 
 -define(APP, folsomite).
 
 event(K, V, Opts) ->
-    event(K, V, node_prefix(), [{tags, get_tags()}|Opts]).
+    event(node_prefix(), K, V, [{tags, get_tags()}|Opts]).
 
 event(Prefix, K, V, Opts) ->
-    event(hostname(), Prefix, K, V, Opts).
+    event(Prefix ++ " " ++ K, V, ok, Opts).
 
-event(Hostname, Prefix, K, V, Opts) ->
-    zeta:ev({Hostname, Prefix ++ " " ++ K}, V, ok, Opts).
+host_event(K, V, Opts) ->
+    host_event(node_prefix(), K, V, [{tags, get_tags()}|Opts]).
+
+host_event(Prefix, K, V, Opts) ->
+    send_event({hostname(), Prefix ++ " " ++ K}, V, ok, Opts).
+
+send_event(Prefix, V, State, Opts) ->
+    zeta:ev(Prefix, V, State, Opts).
 
 hostname() ->
     net_adm:localhost().
